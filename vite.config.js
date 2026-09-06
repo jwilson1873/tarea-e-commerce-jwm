c34f89ded3cbb4a83ee2ee723c6e3c70516e3980
jwilson1873 <jwilson_cl@yahoo.com>
2026-09-05 21:27:37 -0400

    feat: se agrega URL del proyecto para levantarlo en Github Pages

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "https://jwilson1873.github.io/tarea-e-commerce-jwm"
+ })
