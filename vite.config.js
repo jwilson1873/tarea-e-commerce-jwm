16df297447a401962c02f1eb7eab851ff39e8712
jwilson1873 <jwilson_cl@yahoo.com>
2026-09-05 22:00:44 -0400

    fix: se incluyen las imagenes del producto en el build de vite y se ajusta la ruta base vite config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://jwilson1873.github.io/tarea-e-commerce-jwm"
- })
+  base: '/tarea-e-commerce-jwm/',
+  })
